f3476c2a59cff9d8ace8321989e1821e7b9bf506
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2024-05-17 12:08:40 +0100

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,13 @@
+#![deny(missing_docs)]
+#![cfg_attr(test, feature(btree_cursors, assert_matches))]
 
+//! crate with preview of parts of std ( unstable features ).
+
+/// Memory allocation.
+pub mod alloc {}
+
+/// Containers.
+pub mod collections {
+    /// BTreeMap.
+    pub mod btree_map;
+}
