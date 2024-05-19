ccd4ea54fa4d0fb325f1b621d7cbf277ebc1a8c8
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2024-05-19 19:39:55 +0100

    Add files via upload

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![deny(missing_docs)]
 #![cfg_attr(test, feature(btree_cursors, assert_matches))]
 
-//! Crate with parts of Rust std library ( different implementations, features not yet stabilised etc )
+//! Crate with parts of Rust std library ( different implementations, features not yet stabilised etc ), in particular [`collections::btree_map`].
 
 /// Memory allocation.
 pub mod alloc;
