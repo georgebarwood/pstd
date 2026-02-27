d4c7e69f785a6e9b521dfc7c57b90777e5415e1e
georgebarwood <46846948+georgebarwood@users.noreply.github.com>
2026-02-27 18:30:17 +0000

    Add files via upload

--- src/collections.rs
+++ src/collections.rs
@@ -9,6 +9,29 @@ pub use btree_set::BTreeSet;
 mod merge_iter;
 
 /// The error type for `try_reserve` methods.
+#[derive(Debug)]
 pub struct TryReserveError {
-    // kind: TryReserveErrorKind,
+    pub(crate) kind: TryReserveErrorKind,
+}
+
+impl TryReserveError {
+    /// Details about the allocation that caused the error
+    #[must_use]
+    pub fn kind(&self) -> TryReserveErrorKind {
+        self.kind.clone()
+    }
+}
+
+/// Details of the allocation that caused a `TryReserveError`
+#[derive(Debug, Clone)]
+pub enum TryReserveErrorKind {
+    /// Error due to the computed capacity exceeding the collection's maximum
+    /// (usually `isize::MAX` bytes).
+    CapacityOverflow,
+
+    /// The memory allocator returned an error
+    AllocError {
+        /// The layout of allocation request that failed
+        layout: std::alloc::Layout,
+    },
 }
